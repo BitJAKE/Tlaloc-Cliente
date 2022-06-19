ee579fa8040467d9418ef84c650279ec879cc7c2
fernando <ferrafterminador@gmail.com>
2022-06-18 19:59:48 -0600

    primeros diseños responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      'sm': '640px',
+
+      'md': '768px',
+
+      'lg': '1024px',
+
+      'xl': '1280px',
+    }
   },
   plugins: [],
 }

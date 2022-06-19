4f60c33a4f5ab5ad438953e50370d5d016ea0545
fernando <ferrafterminador@gmail.com>
2022-06-19 15:38:42 -0600

    Menu Responsive en móviles

--- src/js/funcionalidades.js
+++ src/js/funcionalidades.js
@@ -1,4 +1,28 @@
 const crearOpcionesNavegacion = () => {
-    console.log("buena")
+    let elemento = document.getElementById("menu")
+    let menuOpciones = document.getElementById("menu-opciones")
+    let content = document.getElementById("content");
+    menuOpciones.innerHTML = ""
+    nombresOpciones = ["Blog", "Eventos", "Productos Eco", "Acerca de"]
+    linkOpciones = ["#", "#", "#", "#"]
+    if (elemento.textContent === "close") {
+        elemento.textContent = "menu"
+        content.className = ""
+        content.className = "inline"
+    } else {
+        content.className = ""
+        content.className = "hidden"
+        for (let i = 0; i < nombresOpciones.length; i++) {
+            elemento.textContent = "close"
+            let link = document.createElement("a")
+            let separador = document.createElement("hr")
+            separador.className = "mt-1 separador"
+            link.textContent = nombresOpciones[i]
+            link.href = linkOpciones[i]
+            link.className = "nav-item block"
+            menuOpciones.appendChild(link)
+            menuOpciones.appendChild(separador)
+        }
+    }
 }
 document.getElementById("menu").addEventListener("click", crearOpcionesNavegacion)

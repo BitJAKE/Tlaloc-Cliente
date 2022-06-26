de5ff72751e2b188d6dc620698042ebb2771ade9
fernando <ferrafterminador@gmail.com>
2022-06-26 00:10:54 -0600

    responsive menu

---
+++ src/js/funcionalidadesPaginas.js
@@ -0,0 +1,29 @@
+const crearOpcionesNavegacion = () => {
+    let elemento = document.getElementById("menu")
+    let menuOpciones = document.getElementById("menu-opciones")
+    let content = document.getElementById("content");
+    menuOpciones.innerHTML = ""
+    nombresOpciones = ["Blog", "Eventos", "Productos Eco", "Acerca de"]
+    linkOpciones = ["../pages/Blog.html", "../pages/Eventos.html", "../pages/Productos.html", "../pages/AboutUs.html"]
+    if (elemento.textContent === "close") {
+        elemento.textContent = "menu"
+        content.className = ""
+        content.className = "inline"
+    } else {
+        content.className = ""
+        content.className = "hidden"
+        for (let i = 0; i < nombresOpciones.length; i++) {
+            elemento.textContent = "close"
+            let link = document.createElement("a")
+            let separador = document.createElement("hr")
+            separador.className = "mt-1 separador"
+            link.textContent = nombresOpciones[i]
+            link.href = linkOpciones[i]
+            link.className = "nav-item block"
+            menuOpciones.appendChild(link)
+            menuOpciones.appendChild(separador)
+        }
+        menuOpciones.className = "mb-36"
+    }
+}
+document.getElementById("menu").addEventListener("click", crearOpcionesNavegacion)
